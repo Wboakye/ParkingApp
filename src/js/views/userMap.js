4826465057b7e54889f467e0bb18be1a0f8ac4ee
William Boakye <wboak001@fiu.edu>
2019-05-10 23:36:19 +0000

    component fix

--- src/js/views/userMap.js
+++ src/js/views/userMap.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
-class SimpleMap extends Component {
-	static defaultProps = {
-		center: {
-			lat: 59.95,
-			lng: 30.33
-		},
-		zoom: 11
-	};
-
+class UserMap extends Component {
 	render() {
 		return (
-			<div className="" style={{ height: "100vh", width: "100%" }}>
+			<div style={{ height: "100vh", width: "100%" }}>
 				<GoogleMapReact
 					style={{ height: "100vh", width: "100%" }}
 					bootstrapURLKeys={{ key: "AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA" }}
@@ -24,4 +16,4 @@ class SimpleMap extends Component {
 	}
 }
 
-export default SimpleMap;
+export default UserMap;
