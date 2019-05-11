2f2347a7bf3c3be54677e0094f358834cf2fb412
William Boakye <wboak001@fiu.edu>
2019-05-11 22:06:27 +0000

    added map brand

--- src/js/views/userMap.js
+++ src/js/views/userMap.js
@@ -11,29 +11,36 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 export class UserMap extends React.Component {
 	render() {
 		return (
-			<div className="row m-0 h-100" id="mapWrapper">
-				<div className="col-sm-12 p-0" id="map" style={{ height: "100vh", width: "100vw" }}>
-					<WrappedMap
-						googleMapURL={
-							"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-						}
-						loadingElement={<div style={{ height: "500px" }} />}
-						containerElement={<div style={{ height: "100%" }} />}
-						mapElement={<div style={{ height: "100%" }} />}
-					/>
-				</div>
-				<div className="col-12 col-sm-3 ml-auto shadow-lg" id="mapInfoWindow">
-					<Link to="/">
-						<button type="button" className="btn btn-outline-light">
-							Home
-						</button>
-					</Link>
+			<div>
+				<nav className="navbar navbar-light bg-transparent fixed-top" id="mapNav">
+					<span className="navbar-brand mb-0 h1">
+						<h1>Park.it</h1>
+					</span>
+				</nav>
+				<div className="row m-0 h-100" id="mapWrapper">
+					<div className="col-sm-12 p-0" id="map" style={{ height: "100vh", width: "100vw" }}>
+						<WrappedMap
+							googleMapURL={
+								"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+							}
+							loadingElement={<div style={{ height: "500px" }} />}
+							containerElement={<div style={{ height: "100%" }} />}
+							mapElement={<div style={{ height: "100%" }} />}
+						/>
+					</div>
+					<div className="col-12 col-sm-3 ml-auto shadow-lg" id="mapInfoWindow">
+						<Link to="/">
+							<button type="button" className="btn btn-outline-light">
+								Home
+							</button>
+						</Link>
+					</div>
 				</div>
 			</div>
 		);
 	}
 }
 
-//https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA
-//https://maps.googleapis.com/maps/api/js?key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA
-//https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places
+// https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA
+// https://maps.googleapis.com/maps/api/js?key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA
+// https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places
