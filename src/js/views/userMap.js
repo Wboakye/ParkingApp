0d0ac06947c8a162c730c0eda096587f92776138
William Boakye <wboak001@fiu.edu>
2019-05-11 21:29:00 +0000

    added info bar to map

--- src/js/views/userMap.js
+++ src/js/views/userMap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
 function Map() {
@@ -10,18 +11,29 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 export class UserMap extends React.Component {
 	render() {
 		return (
-			<div style={{ height: "100vh", width: "100vw" }}>
-				<WrappedMap
-					googleMapURL={
-						"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA"
-					}
-					loadingElement={<div style={{ height: "500px" }} />}
-					containerElement={<div style={{ height: "100%" }} />}
-					mapElement={<div style={{ height: "100%" }} />}
-				/>
+			<div className="row m-0 h-100" id="mapWrapper">
+				<div className="col-sm-12 p-0" id="map" style={{ height: "100vh", width: "100vw" }}>
+					<WrappedMap
+						googleMapURL={
+							"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+						}
+						loadingElement={<div style={{ height: "500px" }} />}
+						containerElement={<div style={{ height: "100%" }} />}
+						mapElement={<div style={{ height: "100%" }} />}
+					/>
+				</div>
+				<div className="col-12 col-sm-3 ml-auto h-100 shadow-lg" id="mapInfoWindow">
+					<Link to="/">
+						<button type="button" className="btn btn-outline-light">
+							Home
+						</button>
+					</Link>
+				</div>
 			</div>
 		);
 	}
 }
 
+//https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA
 //https://maps.googleapis.com/maps/api/js?key=AIzaSyBF7X8agpeC7_q-NEqrXReWCgiZIkHPTrA
+//https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places
