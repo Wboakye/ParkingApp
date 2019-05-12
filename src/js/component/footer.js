975bc0e81523a681496811a1c4b9c36da4ad9ece
William Boakye <wboak001@fiu.edu>
2019-05-12 14:39:25 +0000

    edited footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -5,17 +5,17 @@ export const Footer = () => (
 	<footer className="footer fixed-bottom mt-auto py-3 text-center" id="footerWrapper">
 		<div className="container-fluid">
 			<div className="row justify-content-center">
-				<div className="col-sm-3">
+				<div className="col-4">
 					<i className="fas fa-map-pin fa-2x mt-1 mb-1" />
 					<br />
 					Find a lot.
 				</div>
-				<div className="col-sm-3 mt-1 mb-1">
+				<div className="col-4 mt-1 mb-1">
 					<i className="fas fa-lock fa-2x" />
 					<br />
 					Reserve your spot.
 				</div>
-				<div className="col-sm-3 mt-1 mb-1">
+				<div className="col-4 mt-1 mb-1">
 					<i className="fas fa-parking fa-2x" />
 					<br />
 					Park without stress.
