b677b0cc55e570e517f0c8d4a460fb3487d61324
William Boakye <wboak001@fiu.edu>
2019-05-12 17:50:06 +0000

    in progress map screen search

--- src/js/views/userMap.js
+++ src/js/views/userMap.js
@@ -12,10 +12,29 @@ export class UserMap extends React.Component {
 	render() {
 		return (
 			<div>
-				<nav className="navbar navbar-light bg-transparent fixed-top" id="mapNav">
-					<span className="navbar-brand mb-0 h1">
-						<h1>Park.it</h1>
-					</span>
+				<nav className="navbar navbar-light bg-transparent fixed-top pt-4" id="mapNav">
+					<div className="row" id="searchRowMap">
+						<div className="col-1">
+							<span className="navbar-brand mb-0 h1">
+								<h1>Park.it</h1>
+							</span>
+						</div>
+						<input
+							className="col-6 form-control rounded border border-white ml-3 mt-1 mb-1"
+							type="search"
+							placeholder="Where are we going?"
+							aria-label="Search"
+							id="searchWrapperMap"
+						/>
+
+						<div className="col-1 ml-1 mr-1 p-0">
+							<Link to="/map">
+								<button type="button p-0" className="btn btn-outline-light mt-1 mb-1">
+									Lets Go!
+								</button>
+							</Link>
+						</div>
+					</div>
 				</nav>
 				<div className="row m-0 h-100" id="mapWrapper">
 					<div className="col-sm-12 p-0" id="map" style={{ height: "100vh", width: "100vw" }}>
@@ -29,6 +48,9 @@ export class UserMap extends React.Component {
 						/>
 					</div>
 					<div className="col-12 col-sm-3 ml-auto shadow-lg" id="mapInfoWindow">
+						<div className="row">
+							<div className="col-4" />
+						</div>
 						<Link to="/">
 							<button type="button" className="btn btn-outline-light">
 								Home
